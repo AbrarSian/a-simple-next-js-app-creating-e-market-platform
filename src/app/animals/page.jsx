d71f4927a5c0cb6e3a2982b0bc1f0340227a9755
AbrarSian <abrarsian96@gmail.com>
2026-05-14 02:59:13 +0600

    navbar and footer is done

---
+++ src/app/animals/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Animals = () => {
+    return (
+        <section className='Animals container mx-auto bg-white'>
+            Animals
+        </section>
+    );
+};
+
+export default Animals;

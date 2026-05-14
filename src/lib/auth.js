125b6a29232abae0e5816248df9e95a2ec600482
AbrarSian <abrarsian96@gmail.com>
2026-05-14 23:46:19 +0600

    slight change added

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,9 +1,10 @@
+import dns from "node:dns";
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-
-// 
-// connecting code 
+ 
 const client = new MongoClient(process.env.MONGO_URI);
 
 // db name
